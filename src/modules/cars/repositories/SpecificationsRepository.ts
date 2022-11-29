01a36c7e5439c95a66770a66d56bff41b34d19fb
Angelo Filho <angelo.filho06@hotmail.com>
2022-11-29 16:28:36 -0300

    feat: add clean architecture to specification files

--- src/modules/cars/repositories/SpecificationsRepository.ts
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -6,9 +6,18 @@ import {
 
 class SpecificationRepository implements ISpecificationsRepository {
   private specifications: Specification[];
+  private static INSTANCE: SpecificationRepository;
 
-  constructor() {
+  private constructor() {
     this.specifications = [];
+  }
+
+  public static getInstance(): SpecificationRepository {
+    if (!SpecificationRepository.INSTANCE) {
+      SpecificationRepository.INSTANCE = new SpecificationRepository();
+    }
+
+    return SpecificationRepository.INSTANCE;
   }
 
   create({ description, name }: ICreateSpecificationDTO) {
