91240be84eba369eb55b58ba31dc98f85f5325c5
Angelo Filho <angelo.filho06@hotmail.com>
2022-11-28 21:41:52 -0300

    feat: add validation to category register

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExist = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExist) {
+    return res.status(400).json({ error: "Category Already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
