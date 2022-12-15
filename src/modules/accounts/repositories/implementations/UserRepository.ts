84b01e3f6d856ab2d2179eb901ec396202e40d8b
Angelo Filho <angelo.filho06@hotmail.com>
2022-12-15 19:24:27 -0300

    feat: add create user use case
    - add Create user controller
    - add create user use case
    - add users routes

---
+++ src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -0,0 +1,32 @@
+import { Repository } from "typeorm";
+
+import AppDataSource from "../../../../database/data-source";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUserRepository";
+
+class UserRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(User);
+  }
+
+  async create({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UserRepository };
---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,23 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUserRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUsersRepository
+  ) {}
+
+  async execute({ name, email, password, driver_license }: ICreateUserDTO) {
+    await this.userRepository.create({
+      name,
+      email,
+      password,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
