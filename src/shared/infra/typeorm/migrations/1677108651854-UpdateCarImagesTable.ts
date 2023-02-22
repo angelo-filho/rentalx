5267b1db548087f231342e5207d61c5a7bda0150
Angelo Filho <angelo.filho06@hotmail.com>
2023-02-22 20:53:20 -0300

    feat: add storage provider to cars images

---
+++ src/shared/infra/typeorm/migrations/1677108651854-UpdateCarImagesTable.ts
@@ -0,0 +1,83 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class UpdateCarImagesTable1677108651854 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("cars_image");
+
+    await queryRunner.createTable(
+      new Table({
+        name: "cars_image",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "image_name",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKCarImage",
+            referencedTableName: "cars",
+            referencedColumnNames: ["id"],
+            columnNames: ["car_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("cars_image");
+
+    await queryRunner.createTable(
+      new Table({
+        name: "cars_image",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "image_name",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKCarImage",
+            referencedTableName: "categories",
+            referencedColumnNames: ["id"],
+            columnNames: ["car_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+}
